16140ee05498ec176cbebfeddfc3aedf7da329db
Gabriel <gabriel.avinante4@gmail.com>
2022-12-11 23:39:29 -0700

    added javadoc to wordcounterapp

--- src/application/WordCounterApp.java
+++ src/application/WordCounterApp.java
@@ -7,32 +7,40 @@ import java.util.Scanner;
 import model.HashElement;
 import model.WordCounter;
 
+/**
+ * 
+ * @author Gabriel Avinante & Nathan DeBliek
+ * @version 1.0
+ * Prompts user for a text file and counts overall words and repeated words
+ * Last Modified: December 11 - added javadoc - Gabriel Avinante
+ */
 public class WordCounterApp {
 
 	public static void main(String[] args) throws Exception {
 		Scanner in = new Scanner(System.in);
 		WordCounter test;
-		System.out.print("Enter a txt file(no file type): ");
+		System.out.print("Enter a txt file(no file type): "); //prompts user for file
 		String txt = in.next();
 		File newFile = new File("res/" + txt + ".txt");
 		int size = -1;
 		while(size <= 0) {
-			System.out.print("Enter size for the Hashtable: ");
+			System.out.print("Enter size for the Hashtable: ");//prompts user for size of the hashtable
 			if (in.hasNextInt()) {
 				size = Math.abs(in.nextInt());
 				in.nextLine();
 			} else {
-				System.out.println("Bad Input Please try again: ");
+				System.out.println("Bad Input Please try again: "); //input validation
 			}
 		}
 		test = new WordCounter(size);
 		Scanner file;
 
 		HashElement hash = null;
-		int unique = 0;
-		int count = 0;
 		String temp = null;
 
+		/*
+		 * Reads the inputed file and adds the word strings into the hash table
+		 */
 		try {
 			file = new Scanner(newFile);
 			while (file.hasNext()) {
@@ -42,15 +50,14 @@ public class WordCounterApp {
 				hash.setCount(1);
 				test.put(hash);
 			}
-			unique = count - unique;
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		System.out.println(test.most());
-		System.out.println("Total Words: " + test.countWords());
-		System.out.println("Total Unique Words: " + test.uniqueWords());
-		test.printTable();
+		System.out.println(test.most()); //prints the most reoccuring word
+		System.out.println("Total Words: " + test.countWords()); //prints the total word count
+		System.out.println("Total Unique Words: " + test.uniqueWords()); //prints the total amount of unique words
+		//test.printTable(); //printed the hash table created
 	}
 }
